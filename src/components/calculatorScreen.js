0e0d0c427b8305d9d65095011ad9cdf6ff6077ff
Brett Goers <bgoers@techtonicgroup.com>
2018-08-09 16:31:22 -0600

    changed mindless components to functions

--- src/components/calculatorScreen.js
+++ src/components/calculatorScreen.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-export class CalculatorScreen extends React.Component{
-
-  render(){
-    return(
-        <div className="input-group input-group-lg extra-padded">
-          <input type="text" value={this.props.screenVal} className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
-        </div>)
-  }
+export const CalculatorScreen = function(props){
+  return(
+    <div className="input-group input-group-lg extra-padded">
+      <input type="text" value={props.screenVal} className="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+    </div>
+)
 }
