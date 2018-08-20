c1b27fc60356492f7f7bef8e47af8adb7c05f6cb
Megan M <mmacdonald@techtonicgroup.com>
2018-08-20 08:56:56 -0600

    oulined switch case for handling calculations

--- src/calculator.js
+++ src/calculator.js
@@ -11,17 +11,47 @@ class Calculator extends React.Component {
     this.state = {calcScreen:''}
     this.handleClick = this.handleClick.bind(this)
   }
+
   handleClick(e){
-    if(e.target.value === 'C'){
-      this.clearInput()
-    }else{
-      let calcString = this.state.calcScreen + e.target.value
-      this.setState({calcScreen:calcString})
+    let expression = this.state.expression;
+
+    switch(e.target.value) {
+      case ('C'): this.clearInput();
+      case ('+'): expression = this.state.firstNum +;
+      case ('-'): pass;
+      case ('*'): pass;
+      case ('/'): pass;
+      case ('='): pass;
+      default:
+        let firstNum = this.state.firstNum + e.target.value
+        this.setState({calcScreen})
     }
+
+
+
+
+    // if(e.target.value === 'C'){
+    //   this.clearInput()
+    // }else{
+    //   let calcString = this.state.calcScreen + e.target.value
+    //   this.setState({calcScreen:calcString})
+    // }
   }
+  //switch cases: save number when any of these are pressed
+  //+: add a plus to expression
+  // -: add a minus to expression
+  // *: add *
+  // /:
+  // =: return expression
+
   clearInput(){
     this.setState({calcScreen:''})
   }
+
+  handleCalculation() {
+
+  }
+
   render() {
     return (
       <div className="App">
